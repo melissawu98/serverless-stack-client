cf09b45317aac2b4e33733f5cc2e41b51a3fbe0e
Melissa Wu <melissawu@Melissas-MacBook-Pro.local>
2021-03-03 23:55:57 +1100

    Setting our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "ap-southeast-2",
+      BUCKET: "notes-app-practice",
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://iagx0zy2k6.execute-api.us-east-2.amazonaws.com/prod",
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_19ChamH7e",
+      APP_CLIENT_ID: "15d9qv1iraf5nm632nur7ao6kg",
+      IDENTITY_POOL_ID: "us-east-2:92b102f0-db34-4098-8ed7-7acd8fee72cd",
+    },
+  };
+  
+  export default config;
