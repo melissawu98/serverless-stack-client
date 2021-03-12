1e3400b12c00568f1932c52cdeaa1f3491a1412d
Melissa Wu <melissawu@Melissas-MacBook-Pro.local>
2021-03-12 17:01:26 +1100

    Updating the lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,6 +4,7 @@ import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import { API } from "aws-amplify";
 import { BsPencilSquare } from "react-icons/bs";
+import { Link } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import "./Home.css";
 
@@ -63,12 +64,20 @@ export default function Home() {
 
     function renderLander() {
         return (
-            <div className="lander">
-                <h1>Scratch</h1>
-                <p className="text-muted">A simple note taking app</p>
+          <div className="lander">
+            <h1>Scratch</h1>
+            <p className="text-muted">A simple note taking app</p>
+            <div className="pt-3">
+              <Link to="/login" className="btn btn-info btn-lg mr-3">
+                Login
+              </Link>
+              <Link to="/signup" className="btn btn-success btn-lg">
+                Signup
+              </Link>
             </div>
+          </div>
         );
-    }
+      }
 
     function renderNotes() {
         return (
